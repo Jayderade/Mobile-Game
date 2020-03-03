9f848e54c76a28c94e7cbf5f741d0bee7cdfa2f9
Jayderade <30771579+Jayderade@users.noreply.github.com>
2020-03-03 13:37:28 +1100

    1st push

---
+++ Assets/MobileInput.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInput : MonoBehaviour
+{
+    [Header("Accelerometer")]
+    public float speed = 10;
+    public bool tilt = true;
+    [Header("Pinch Zoom")]
+    public float perspectiveZoomSpeed = .5f;
+    public float orthographicZoomSpeed = .5f;
+    
+    void Update()
+    {
+        if(tilt)
+        {
+            float accel = speed * Time.deltaTime;
+            Accelerometer(accel);
+        }
+        PinchZoom();
+    }
+
+    void Accelerometer(float accel)
+    {
+        transform.Translate(Input.acceleration.x * accel, 0, Input.acceleration.z * accel);
+    }
+
+    void PinchZoom()
+    {
+        //If we have 2 fingers on screen
+        if(Input.touchCount == 2)
+        {
+            //First touch input on screen
+            Touch touchZero = Input.GetTouch(0);
+            //Second touch input on screen
+            Touch touchOne = Input.GetTouch(1);
+            //Difference between current and previous positions (positive or negative value)
+            Vector2 touchZeroPreviousPosition = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePreviousPosition = touchOne.position - touchOne.deltaPosition;
+            //Magnitude difference between the first touches on screen
+            float previousTouchMagitude = (touchZeroPreviousPosition - touchOnePreviousPosition).magnitude;
+            //Magnitude difference between the current touches on screen
+            float currentTouchMagnitude = (touchZero.position - touchOne.position).magnitude;
+            //Magnitude difference between the 2
+            float magnitudeDifference = previousTouchMagitude - currentTouchMagnitude;
+
+            if(Camera.main.orthographic)
+            {
+                //Zoom in or out depending on magnitude
+                Camera.main.orthographicSize += magnitudeDifference * orthographicZoomSpeed;
+                Camera.main.orthographicSize = Mathf.Max(Camera.main.orthographicSize,0.1f);
+            }
+            else
+            {
+                //Zoom in or out depending on magnitude
+                Camera.main.fieldOfView += magnitudeDifference * perspectiveZoomSpeed;
+
+                Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 0.1f, 180);
+            }
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+    [Header("Keys")]
+    public GameObject firstKey;
+    public GameObject secondKey;
+    public GameObject thirdKey;
+    public GameObject lastKey;
+    [Header("Doors")]
+    public GameObject firstDoor;
+    public GameObject secondDoor;
+    public GameObject thirdDoor;
+    public GameObject lastDoor;
+    public GameObject currentDoor;
+
+    public void OnCollisionEnter(Collision key)
+    {
+       if(key.gameObject.tag == "Key" )
+        {
+            key.gameObject.SetActive(false);
+            currentDoor.SetActive(true);
+
+            if(currentDoor == firstDoor)
+            {
+                currentDoor = secondDoor;
+            }
+            if (currentDoor == secondDoor)
+            {
+                currentDoor = thirdDoor;
+            }
+            if (currentDoor == thirdDoor)
+            {
+                currentDoor = lastDoor;
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDoor = firstDoor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
