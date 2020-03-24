e5b512bb589f1efb89185b7b11c7cc4a2600797c
Jayderade <30771579+Jayderade@users.noreply.github.com>
2020-03-24 13:39:40 +1100

    bluh

---
+++ Assets/PhysicsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsController : MonoBehaviour
+{
+    public float shakeForceMultiplier;
+    public Rigidbody2D[] shakeableRigidbodys;
+
+    public void ShakeRigidbodies(Vector3 deviceAcceleration)
+    {
+        foreach( var rigidbody in shakeableRigidbodys)
+        {
+            rigidbody.AddForce(deviceAcceleration * shakeForceMultiplier, ForceMode2D.Impulse);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ShakeDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PhysicsController))]
+public class ShakeDetector : MonoBehaviour
+{
+    public float shakeDetectionThreshold;
+    public float miniShakeInterval;
+    public float sqrShakeDetectionThreshold;
+    public float timeSinceLastShake;
+    public PhysicsController physicsController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sqrShakeDetectionThreshold = Mathf.Pow(shakeDetectionThreshold, 2);
+        physicsController = GetComponent<PhysicsController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.acceleration.sqrMagnitude >= shakeDetectionThreshold && Time.unscaledTime >= timeSinceLastShake + miniShakeInterval)
+        {
+            physicsController.ShakeRigidbodies(Input.acceleration);
+            timeSinceLastShake = Time.unscaledTime;
+        }
+    }
+}
